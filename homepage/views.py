2736f07d72a71b0c61e8118c0d683fbeac10832a
Ambesh Tiwari <ambesht@amazon.com>
2020-11-06 03:33:06 +0530

    Adding blog (static template)

--- homepage/views.py
+++ homepage/views.py
@@ -3,4 +3,5 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
