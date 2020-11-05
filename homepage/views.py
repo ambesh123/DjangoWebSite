2f2a83402a3a0d62deb23af265dc22f6771798bd
Ambesh Tiwari <ambesht@amazon.com>
2020-11-06 03:53:24 +0530

    Modified to pick posts from database

--- homepage/views.py
+++ homepage/views.py
@@ -2,6 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
