87be471abcba9ce23b99b90e17bb7956eef34cba
Ambesh Tiwari <ambesht@amazon.com>
2020-11-08 13:53:32 +0530

    Added page for individual posts

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('posts/', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -8,3 +8,7 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
