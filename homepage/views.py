81270cfbcff1db55c3b64dae55bbfc92f7c2a658
Ambesh Tiwari <ambesht@amazon.com>
2020-11-09 02:16:48 +0530

    Adding comment model to the blog

--- homepage/views.py
+++ homepage/views.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from django.utils import timezone
-from .new_post import PostForm
+from .forms import PostForm, CommentForm
 
 def home(request):
     #Currently we don't have a homepage so we'll redirect user to blog
@@ -44,3 +44,16 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
